e4a11d6308914947f46620d277844b7d3a3e54d6
Yamkela Zungula <zungulay@gmail.com>
2026-09-17 14:52:11 +0200

    Add tailiwindcss package and removed some boilerplate code

--- work-order-vue-app/src/views/HomeView.vue
+++ work-order-vue-app/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-3xl text-amber-600">Work Orders Tracker</h1>
   </main>
 </template>
+
+<script setup lang="ts"></script>
